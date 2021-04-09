7c16457621d2619ff2bb863bce3d35864cf795e8
phamtrieuduyminh <phamtrieuduyminh1@gmail.com>
2021-04-09 22:44:46 +0700

    xong bai tap ngay 2

---
+++ buoi2/js/bai2.js
@@ -0,0 +1,47 @@
+var noidung = [{
+    name: "Unlimited Color",
+    image: "images/UnlimitedColor.png",
+    text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
+}, {
+    name: "Unlimited Color",
+    image: "images/UnlimitedColor.png",
+    text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
+
+
+}, {
+    name: "Unlimited Color",
+    image: "images/UnlimitedColor.png",
+    text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
+
+
+}, {
+    name: "Unlimited Color",
+    image: "images/UnlimitedColor.png",
+    text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
+
+
+}, {
+    name: "Unlimited Color",
+    image: "images/UnlimitedColor.png",
+    text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
+
+
+}, {
+    name: "Unlimited Color",
+    image: "images/UnlimitedColor.png",
+    text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
+
+
+}, {
+    name: "Unlimited Color",
+    image: "images/UnlimitedColor.png",
+    text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
+
+
+}, {
+    name: "Unlimited Color",
+    image: "images/UnlimitedColor.png",
+    text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
+
+
+}]
