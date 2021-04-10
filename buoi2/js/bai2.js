808e8c890a9b14ec9c86b22f1e523041616871cd
phamtrieuduyminh <phamtrieuduyminh1@gmail.com>
2021-04-10 19:11:01 +0700

    capnhat js

--- buoi2/js/bai2.js
+++ buoi2/js/bai2.js
@@ -1,11 +1,31 @@
 var noidung = [{
-    name: "Unlimited Color",
-    image: "images/UnlimitedColor.png",
-    text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
-}, {
-    name: "Unlimited Color",
-    image: "images/UnlimitedColor.png",
-    text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
+        name: "Unlimited Color",
+        image: "images/UnlimitedColor.png",
+        text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
+    }, {
+        name: "Unlimited Color",
+        image: "images/UnlimitedColor.png",
+        text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
 
 
-}]
+    }, {
+        name: "Unlimited Color",
+        image: "images/UnlimitedColor.png",
+        text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
+    },
+    {
+        name: "Unlimited Color",
+        image: "images/UnlimitedColor.png",
+        text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
+    },
+    {
+        name: "Unlimited Color",
+        image: "images/UnlimitedColor.png",
+        text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
+    },
+    {
+        name: "Unlimited Color",
+        image: "images/UnlimitedColor.png",
+        text: "with color picker in admin panel, you can make your site more dynamic. Youcan choose a color from popup with one click or paste your code into fleld"
+    },
+]
