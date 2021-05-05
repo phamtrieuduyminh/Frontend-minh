24a8b40b489c76e28e14f8e09a42f9ae844f8435
phamtrieuduyminh <phamtrieuduyminh1@gmail.com>
2021-05-05 21:01:36 +0700

    aA

--- js/bai9.js
+++ js/bai9.js
@@ -12,7 +12,7 @@ app.controller("myctrl9", function($scope) {
             var newNumber = [];
 
             for (var i = 0; i < 15; i++) {
-                newNumber = Math.floor(Math.random() * (200 - (-200))) + -200;
+                newNumber = Math.round(Math.random() * (200 - (-200))) + -200;
                 soNN.push(newNumber);
             }
 
